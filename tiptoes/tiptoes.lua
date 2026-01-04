9c446d191f78fea2628f6c217258f4478b153e76
lili-ffxi <40600148+lili-ffxi@users.noreply.github.com>
2026-01-04 14:55:25 +0000

    Update tiptoes.lua
    Medium refactor, added support for Evanessence and Rainbow Powder, made some preparations to be able to support equippable gear with Sneak/Invisible enchantments.

--- tiptoes/tiptoes.lua
+++ tiptoes/tiptoes.lua
@@ -1,7 +1,7 @@
 _addon.name = 'Tiptoes'
 _addon.author = 'Lili'
-_addon.version = '0.0.9'
-_addon.commands = {'tiptoes','tt'}
+_addon.version = '1.1.0'
+_addon.commands = {'tiptoes', 'tt'}
 
 require('logger')
 require('pack')
@@ -12,10 +12,12 @@ local queue = {}
 
 local verbose = false
 
+local wc_match = windower.wc_match
+
 function dbg() table.vprint(queue) end
 
 function cmd(str)
-    if str:sub(1,1) == '/' then
+    if str:sub(1, 1) == '/' then
         windower.chat.input(str)
     else
         windower.send_command(str)
@@ -30,34 +32,87 @@ local argdict = {
     ['in'] = {'invisible'},
     invis = {'invisible'},
     invisible = {'invisible'},
-    si = {'sneak','invisible'},
-    snin = {'sneak','invisible'},
-    sneakinv = {'sneak','invisible'},
-    sneakinvis = {'sneak','invisible'},
-    both = {'sneak','invisible'},
+    si = {'sneak', 'invisible'},
+    snin = {'sneak', 'invisible'},
+    sneakinv = {'sneak', 'invisible'},
+    sneakinvis = {'sneak', 'invisible'},
+    both = {'sneak', 'invisible'},
 }
 
 -- can change priorities here by simply reordering the lines.
 -- TODO: configurable priorities.
 local actions = {
     sneak = {
-        {job='DNC',type='ja',name='Spectral Jig',level=25,command='/ja "Spectral Jig" <me>'},
-        {job='WHM',type='ma',name='Sneak',level=20,command='/ma Sneak <me>'},
-        {job='RDM',type='ma',name='Sneak',level=20,command='/ma Sneak <me>'},
-        {job='SCH',type='ma',name='Sneak',level=20,command='/ma Sneak <me>'},
-        {job='NIN',type='ma',name='Monomi: Ichi',level=25,command='/ma "Monomi:ichi" <me>'},
-        item = {type='item',name='Silent Oil',command='/item "Silent Oil" <me>'},
+        { job='DNC',         type='ja',   name='Spectral Jig',   level=25, },
+        { job='WHM|RDM|SCH', type='ma',   name='Sneak',          level=20, },
+        { job='NIN',         type='ma',   name='Monomi: Ichi',   level=25, },
+        { job='???',         type='item', name='Silent Oil',     level=1,  },
+        { job='???',         type='item', name='Evanessence',    level=1,  },
     },
     invisible = {
-        {job='DNC',type='ja',name='Spectral Jig',level=25,command='/ja "Spectral Jig" <me>'},
-        {job='WHM',type='ma',name='Invisible',level=25,command='/ma Invisible <me>'},
-        {job='RDM',type='ma',name='Invisible',level=25,command='/ma Invisible <me>'},
-        {job='SCH',type='ma',name='Invisible',level=25,command='/ma Invisible <me>'},
-        {job='NIN',type='ma',name='Tonko: Ni',level=9,command='/ma "Tonko: Ni" <me>'},
-        {job='NIN',type='ma',name='Tonko: Ichi',level=9,command='/ma "Tonko: Ichi" <me>'},
-        item = {type='item',name='Prism Powder',command='/item "Prism Powder" <me>'},
+        { job='DNC',         type='ja',   name='Spectral Jig',   level=25, },
+        { job='WHM|RDM|SCH', type='ma',   name='Invisible',      level=25, },
+        { job='NIN',         type='ma',   name='Tonko: Ni',      level=9,  },
+        { job='NIN',         type='ma',   name='Tonko: Ichi',    level=9,  },
+        { job='???',         type='item', name='Prism Powder',   level=1,  },
+        { job='???',         type='item', name='Rainbow Powder', level=1,  },
+        { job='???',         type='item', name='Evanessence',    level=1,  },
     },
 }
+
+function action_available(action)
+    local player = windower.ffxi.get_player()
+    if verbose then
+        log(player.main_job, player.main_job_level, player.sub_job, player.sub_job_level)
+    end
+
+    if not (wc_match(player.main_job, action.job) and player.main_job_level >= action.level) and
+        not (wc_match(player.sub_job, action.job) and player.sub_job_level >= action.level) then
+            return false
+    end
+
+    local recasts = {}
+    local recast_id
+    if action.type == 'ja' then
+        recasts = windower.ffxi.get_ability_recasts()
+        recast_id = res.job_abilities:with('name', action.name).recast_id
+
+        return recasts[recast_id] == 0
+
+    elseif action.type == 'ma' then
+        local spell_id = res.spells:with('name', action.name).id
+        local spells = windower.ffxi.get_spells()
+        recasts = windower.ffxi.get_spell_recasts()
+        recast_id = spells[spell_id] and spell_id
+
+        return recasts[recast_id] == 0
+
+    elseif action.type == 'item' then
+        local item_id = res.items:with('name', action.name).id
+        local bags = res.bags:filter(function(t) return t.access == 'Everywhere' and t.en ~= "Recycle" end)
+
+        for _, bag in pairs(bags) do
+            local items = windower.ffxi.get_items(bag.id)
+            for slot, item in ipairs(items) do
+                if item.id > 0 and item.id == item_id then
+                    if verbose then
+                        log('found', item.id, res.bags[bag.id].en, action.name)
+                    end
+
+                    action.item_id = item.id
+                    action.bag = bag.id
+
+                    return true
+                end
+            end
+        end
+
+    elseif action.type == 'gear' then
+        -- todo
+    end
+
+    return false
+end
 
 function queue_action(...)
     queue[#queue+1] = ...
@@ -65,48 +120,24 @@ function queue_action(...)
 end
 
 function work(job)
-    local player = windower.ffxi.get_player()
-    if verbose then log(player.main_job,player.main_job_level,player.sub_job,player.sub_job_level) end
-    
-    for _,task in ipairs(job) do
+    for _, task in ipairs(job) do
         if actions[task] then
-            if verbose then log('task:',task) end
+            if verbose then
+                log('task:', task)
+            end
+            
             local result = false
-            for _,action in ipairs(actions[task]) do
-                if (player.main_job == action.job and player.main_job_level >= action.level) or
-                    (player.sub_job == action.job and player.sub_job_level >= action.level) then
-                    local recasts local recast_id
-                    if action.type == 'ja' then
-                        recasts = windower.ffxi.get_ability_recasts()
-                        recast_id = res.job_abilities:with('name',action.name).recast_id
-                    elseif action.type == 'ma' then
-                        local spell_id = res.spells:with('name',action.name).id
-                        local spells = windower.ffxi.get_spells()
-                        recasts = windower.ffxi.get_spell_recasts()
-                        recast_id = spells[spell_id] and spell_id
-                    end
-                    if recasts[recast_id] == 0 then
-                        result = table.copy(action)
-                    end
+            for _, action in ipairs(actions[task]) do
+                if action_available(action) then
+                    action.command = '/%s "%s" <me>':format(action.type, action.name)
+                    result = table.copy(action)
+                    
                     break
                 end
             end
-            if not result then
-                local item_id = res.items:with('name',actions[task].item.name).id
-                local bags = res.bags:filter(function(t) return t.access == 'Everywhere' and t.en ~= "Recycle" end)
-                for _,bag in pairs(bags) do
-                    local items = windower.ffxi.get_items(bag.id)
-                    for slot,item in ipairs(items) do
-                        if item.id > 0 and item.id == item_id then
-                            if verbose then log('found',item.id,res.bags[bag.id].en,actions[task].item.name) end
-                            result = table.update(table.copy(actions[task].item),{item_id=item.id,bag=bag.id})
-                            break
-                        end
-                    end
-                end
-            end
+
             if result then
-                queue_action(table.update(result,{['task']=task:ucfirst()})) -- 3am hack that I will certainly never ever regret
+                queue_action(table.update(result, {['task']=task:ucfirst()})) -- 3am hack that I will certainly never ever regret
                 next_sequence = 0
             end
         end
@@ -117,13 +148,14 @@ local midaction = function()
     local acting = false
     local last_action = -1
     local cooldown = false
-    
+
     return function(param)
         if param ~= nil then
             acting = param and true
             cooldown = type(param) == 'number' and param > 0 and param
             last_action = os.clock()
         end
+
         if cooldown and os.clock() > (last_action + cooldown) then
             cooldown = false
             acting = false
@@ -138,14 +170,14 @@ windower.register_event('addon command', function(...)
         print('Tiptoes: not logged in.')
         return
     end
-    
+
     local args = {...}
-    
+
     local arg = args[1] and args[1]:lower() or 'both'
-    local job     
-    
-    if arg == 'r' then 
-        cmd('lua r tiptoes') 
+    local job
+
+    if arg == 'r' then
+        cmd('lua r tiptoes')
         return
     elseif arg == 'help' then
         log('v.'.._addon.version)
@@ -154,18 +186,22 @@ windower.register_event('addon command', function(...)
         log('commands can be shortened to s sn i in si snin')
         log('if no argument or an invalid argument is passed, I will apply both Sneak and Invisible.')
         return
+    elseif arg == 'verbose' then
+        verbose = not verbose
+        log('Verbose is now ' .. tostring(verbose))
+        return
     elseif argdict[arg] then
         job = argdict[arg]
     else
         log(arg:color(64), 'is an invalid command. Performing default action.')
         job = argdict.both
     end
-        
+
     work(job)
-    
+
 end)
 
-windower.register_event('incoming chunk',function(id,org,mod,inj,blk)
+windower.register_event('incoming chunk', function(id, org, mod, inj, blk)
     if not active then
         return
     end
@@ -191,17 +227,17 @@ windower.register_event('incoming chunk',function(id,org,mod,inj,blk)
 end)
 
 local moving = false
-windower.register_event('outgoing chunk',function(id,org,mod,inj,blk)
+windower.register_event('outgoing chunk', function(id, org, mod, inj, blk)
     if not active then
         return
     end
 
-    local seq = org:unpack('H',3)
-    
+    local seq = org:unpack('H', 3)
+
     if id == 0x015 then
         moving = lastlocation ~= mod:sub(5, 16)
         lastlocation = mod:sub(5, 16)
-        if not next_sequence then 
+        if not next_sequence then
             next_sequence = (seq+5)%0x10000 -- 128 packets is about 1 minute. 5 packets is about 2 seconds.
         end
     end
@@ -210,17 +246,19 @@ windower.register_event('outgoing chunk',function(id,org,mod,inj,blk)
         if windower.ffxi.get_player().status > 1 or moving or midaction() then
             return
         end
-        
+
         next_sequence = nil
-        
+
         while #queue > 0 do
             local buffactive = S(windower.ffxi.get_player().buffs):map(function(id) return res.buffs[id].name end)
 
-            local action = table.remove(queue,1)
+            local action = table.remove(queue, 1)
+
             if verbose then
                 table.print(action)
-                log(action.task,buffactive[action.task])
+                log(action.task, buffactive[action.task])
             end
+
             if not buffactive[action.task] then
                 if action.type == 'item' and action.bag > 0 then -- need pull
                     local inventory = windower.ffxi.get_bag_info(0)
@@ -230,37 +268,43 @@ windower.register_event('outgoing chunk',function(id,org,mod,inj,blk)
                         return
                     end
                     local bag = windower.ffxi.get_items(action.bag)
-                    for slot,item in pairs(bag) do
+                    for slot, item in pairs(bag) do
                         if item.id == action.item_id then
-                            windower.ffxi.get_item(action.bag,slot,1)
-                            table.insert(queue,1,table.update(action,{retry=true,bag=0}))
+                            windower.ffxi.get_item(action.bag, slot, 1)
+                            table.insert(queue, 1, table.update(action, {retry=true, bag=0}))
                             next_sequence = nil
                             return
                         end
                     end
-                elseif action.name == 'Spectral Jig' and buffactive['Sneak'] then
+                elseif (action.name == 'Spectral Jig' or action.name == 'Evanessence') and buffactive['Sneak'] then
                     if action.retry then
                         error('Cannot cancel Sneak, giving up.')
                         return
                     end
                     log('Sneak active, attempting to cancel the buff...')
                     cmd('cancel sneak')
-                    table.insert(queue,1,table.update(action,{retry=true}))
+                    table.insert(queue, 1, table.update(action, {retry=true}))
                     return
                 end
+
                 log('using %s.':format(action.name))
+
+                if action.name == 'Sneak' or action.name == 'Invisible' then
+                    cmd('accession')
+                    coroutine.sleep(1.1)
+                end
                 cmd(action.command)
                 return
             end
         end
-        
+
         active = false
-        
+
     end
-    
+
 end)
 
-windower.register_event('zone change','job change','logout','unload',function()
+windower.register_event('zone change', 'job change', 'logout', 'unload', function()
     active = false
     queue = {}
 end)
